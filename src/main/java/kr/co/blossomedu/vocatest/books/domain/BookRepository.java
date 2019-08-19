9befe647187aa28588d69e2b0a113dc185925246
siyoon <siyoon210@gmail.com>
2019-08-19 21:40:49 +0900

    refactor: 카테시안 곱 문제로 조회해오는 북이 15개가 되어 DISTNCT 설정

--- src/main/java/kr/co/blossomedu/vocatest/books/domain/BookRepository.java
+++ src/main/java/kr/co/blossomedu/vocatest/books/domain/BookRepository.java
@@ -2,10 +2,12 @@ package kr.co.blossomedu.vocatest.books.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    @Query("SELECT b FROM Book b JOIN FETCH b.chapters")
+    @Query("SELECT DISTINCT b FROM Book b JOIN FETCH b.chapters bc")
     List<Book> findAll();
 }
